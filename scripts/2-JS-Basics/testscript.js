0bd5e911fd133633339a1f50a2ad8e3feb0eb2a3
herpderpqa <30484549+herpderpqa@users.noreply.github.com>
2017-07-31 07:24:51 -0700

    d
    d

---
+++ scripts/2-JS-Basics/testscript.js
@@ -0,0 +1,31 @@
+/*var name = 'John Waffles';
+console.log(name);
+
+var lastName = 'Smith Waffles';
+console.log(lastName);
+
+var age = 25;
+console.log(age);
+
+var fullAge = true; 
+console.log(fullAge);
+*/
+
+//Lecture: Variables 2 
+
+var name = 'John Bacon';
+var age = 32;
+
+console.log(name +" "+ age);
+console.log(age+age);
+
+var job, isMarried;
+
+console.log(job);
+
+job = "Web Dev";
+isMarried = false;
+if(isMarried == true){console.log(job);
+}else{
+	console.log("Fail ure cauhgt")
+}
